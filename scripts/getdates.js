b82997ed98717b9d7c802ea040f639360ecd2d58
erin-major <erinmajor254@gmail.com>
2024-04-27 15:41:16 -0600

    Save changes

---
+++ scripts/getdates.js
@@ -0,0 +1,7 @@
+const year = document.querySelector("#currentyear");
+const modifiedDate = document.querySelector("#lastModified");
+
+const today = new Date();
+
+year.innerHTML = `©️ <span id="currentyear">${today.getFullYear()}</span> | Erin Major | Utah, United States`;
+modifiedDate.innerHTML = `Last Modification: ${today}`;
